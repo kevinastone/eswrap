744657ed94d8c388f7d3d77a2ba6731e77669def
Kevin Stone <kevinastone@gmail.com>
2016-12-10 17:00:17 -0800

    Added template literals

---
+++ tests/literals.spec.js
@@ -0,0 +1,30 @@
+import 'mocha';
+import 'chai';
+
+import transform from './../index';
+
+describe('Literals', () => {
+  describe('NumericLiterals', () => {
+    it(`shouldn't wrap literals`, () => {
+      expect(transform(`var a = 20;`, 20)).toEqual(`var a = 20;`);
+    });
+  });
+  describe('StringLiterals', () => {
+    it(`shouldn't wrap literals`, () => {
+      expect(transform(`var a = 'hi';`, 20)).toEqual(`var a = 'hi';`);
+    });
+  });
+  describe('TemplateLiterals', () => {
+    it(`shouldn't wrap literals`, () => {
+      expect(transform('var a = `hi`;', 20)).toEqual('var a = `hi`;');
+    });
+    it(`shouldn't wrap literals with expressions`, () => {
+      // eslint-disable-next-line
+      expect(transform('var a = `hi ${ person}`;', 20)).toEqual('var a = `hi ${person}`;');
+    });
+    it(`shouldn't wrap literals with multiple expressions`, () => {
+      // eslint-disable-next-line
+      expect(transform('var a = `hi ${person}, good ${timeofday()}`;', 50)).toEqual('var a = `hi ${person}, good ${timeofday()}`;');
+    });
+  });
+});
