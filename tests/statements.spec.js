2ddf5919ca169816d151117161eec5d84d124aa2
Kevin Stone <kevinastone@gmail.com>
2016-12-13 21:36:23 -0800

    Added support for while statement

--- tests/statements.spec.js
+++ tests/statements.spec.js
@@ -15,7 +15,7 @@ describe('Statements', () => {
     });
   });
   describe('If Statements', () => {
-    it(`shouldn't wrap if statements if under lenth`, () => {
+    it(`shouldn't wrap if statements if under length`, () => {
       expect(transform(`if (a == true) {}`, 20)).toEqual(dedent`
         if (a == true) {}
       `);
@@ -28,8 +28,22 @@ describe('Statements', () => {
       `);
     });
   });
+  describe('While Statements', () => {
+    it(`shouldn't wrap while statements while under length`, () => {
+      expect(transform(`while (a == true) {}`, 20)).toEqual(dedent`
+        while (a == true) {}
+      `);
+    });
+    it(`should wrap while statements at the binary expressions`, () => {
+      expect(transform(`while (something || another || more) {}`, 20)).toEqual(dedent`
+        while (something ||
+          another ||
+          more) {}
+      `);
+    });
+  });
   xdescribe('For of Statements', () => {
-    it(`shouldn't wrap for-of statements if under lenth`, () => {
+    it(`shouldn't wrap for-of statements if under length`, () => {
       expect(transform(`for (const a of []) {}`, 30)).toEqual(dedent`
         for (const a of []) {}
       `);
