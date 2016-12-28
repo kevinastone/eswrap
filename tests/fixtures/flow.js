fa8073181919fd2720f4b0792a74bed6bfc558b4
Kevin Stone <kevinastone@gmail.com>
2016-12-28 14:52:28 -0600

    Prevent splitting on type parameters

--- tests/fixtures/flow.js
+++ tests/fixtures/flow.js
@@ -1,7 +1,7 @@
 // @input
 type Generator<T> = () => Iterable<T>;
 export function* product<T>(...iterables: Array<Generator<T>>): Iterable<Array<T>> {}
-// @expect length=80 skip # need to fix solve order
+// @expect length=80
 type Generator<T> = () => Iterable<T>;
 export function* product<T>(...iterables: Array<Generator<T>>)
   : Iterable<Array<T>> {}
